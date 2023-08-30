40bef30e92d1171308fe7348214c480cc17609c8
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-08-30 15:43:18 -0600

    finished movie model

--- src/model.js
+++ src/model.js
@@ -32,3 +32,50 @@ User.init(
     sequelize: db,
   },
 );
+
+export class Movie extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Movie.init(
+  {
+    movieId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    overview: {
+      type: DataTypes.TEXT,
+    },
+    releaseDate: {
+      type: DataTypes.DATE,
+    },
+    posterPath: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    modelName: 'movie',
+    sequelize: db,
+  },
+);
+
+//Use these four commands in the Node REPL to test the movie model.
+
+// const { Movie, db } = await import('./src/model.js');
+
+// await db.sync();
+
+// const testMovie = await Movie.create({ title: 'Test Movie' });
+
+// console.log(testMovie);
+
+
+
+
