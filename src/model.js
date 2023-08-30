363c3cc060cc744c5ef03541b3abf07f36cedf4d
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-08-30 15:29:20 -0600

    2nd commit

---
+++ src/model.js
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from 'sequelize';
+import util from 'util';
+import connectToDB from './db.js';
+
+export const db = await connectToDB('postgresql:///ratings');
+
+export class User extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+User.init(
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    modelName: 'user',
+    sequelize: db,
+  },
+);
